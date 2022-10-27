696389f778f1ab896886fd942bc33c1b9dc65f2b
Manish0114 <116781314+Manish0114@users.noreply.github.com>
2022-10-27 09:56:54 +0530

    testing

---
+++ perimeter.py
@@ -0,0 +1,13 @@
+25-10-2022
+
+
+write a program to find the area and perimeter of a square?
+
+side = float(input("Enter side of a square"))
+area = 4*side
+print("perimeter of a square is", area)
+
+
+side = float(input("Enter side of a square"))
+area = 4*side
+print("perimeter of a square is", 4*side)
